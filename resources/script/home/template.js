1c0d227c911c9791feba7d95f84c149deb591cff
Oscar Andrés Ochoa Villegas <oscarochoa@process-on-line.com>
2018-10-18 08:28:04 -0500

    Commit with added initial math content

--- resources/script/home/template.js
+++ resources/script/home/template.js
@@ -13,7 +13,13 @@ $(function() {
         $('#a_structures_stack').attr("href", relative + 'web/sistemas/java/pilas.html');
         $('#a_structures_queue').attr("href", relative + 'web/sistemas/java/colas.html');
         $('#a_intro_math').attr("href", relative + 'web/matematicas/intro_matematicas.html');
+        $('#a_math_operativas').attr("href", relative + 'web/matematicas/basica/operativas.html');
+        $('#a_math_derivadas').attr("href", relative + 'web/matematicas/calculo/derivadas.html');
+        $('#a_math_integral').attr("href", relative + 'web/matematicas/calculo/integral.html');
         $('#a_math_limites').attr("href", relative + 'web/matematicas/calculo/limites.html');
+        $('#a_math_series').attr("href", relative + 'web/matematicas/calculo/series.html');
+        $('#a_math_vectorial').attr("href", relative + 'web/matematicas/geometria/vectorial.html');
+        $('#a_math_matrices').attr("href", relative + 'web/matematicas/lineal/matrices.html');
     });
     $('#footer').load(relative + 'footer.html');
 });
